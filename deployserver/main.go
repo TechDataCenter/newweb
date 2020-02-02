f2c1b7eb03f8317783d419aaddcaaa559e931254
duke <duke@chope.co>
2020-02-02 13:53:29 +0800

    code fix

--- deployserver/main.go
+++ deployserver/main.go
@@ -17,7 +17,7 @@ func reLaunch() {
 }
 
 func firstPage(w http.ResponseWriter, r *http.Request)  {
-	io.WriteString(w,"<h1>Hello,this is my firstpage!</h1>")
+	io.WriteString(w,"<h1>Hello,this is my deploy server!</h1>")
 	reLaunch()
 }
 
