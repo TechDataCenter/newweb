4dc4207504ab3aa842eb615822f002620f245654
duke <duke@chope.co>
2020-02-02 14:19:50 +0800

    update i am duke

--- webserver/main.go
+++ webserver/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func firstPage(w http.ResponseWriter, r *http.Request)  {
-	io.WriteString(w,"<h1>Hello,this is my firstpage update!</h1>")
+	io.WriteString(w,"<h1>Hello, I am duke!</h1>")
 }
 
 func main() {
