c3092f60fe53a933390d4f53d2975eeb82577210
duke <duke@chope.co>
2020-02-02 14:24:35 +0800

    update

--- webserver/main.go
+++ webserver/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func firstPage(w http.ResponseWriter, r *http.Request)  {
-	io.WriteString(w,"<h1>Hello, I am duke!</h1>")
+	io.WriteString(w,"<h1>Hello, 彭琳琳!</h1>")
 }
 
 func main() {
